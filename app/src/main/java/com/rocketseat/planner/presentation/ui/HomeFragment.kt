b7e2ab0b6eeb34901982b0f300ed9b94a796d5d6
Egito Filho <egitofilho4@gmail.com>
2025-01-07 22:53:37 -0300

    04_37_IMPLEMENTANDO_A_LOGICA_PARA_ADICIONAR_ATIVIDADES_PARTE_02

--- app/src/main/java/com/rocketseat/planner/presentation/ui/HomeFragment.kt
+++ app/src/main/java/com/rocketseat/planner/presentation/ui/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.rocketseat.planner.presentation.ui
 
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -13,10 +15,16 @@ import com.google.android.material.snackbar.Snackbar
 import com.rocketseat.planner.R
 import com.rocketseat.planner.databinding.FragmentHomeBinding
 import com.rocketseat.planner.domain.utils.imageBase64ToBitmap
+import com.rocketseat.planner.domain.utils.toPlannerActivityDate
+import com.rocketseat.planner.domain.utils.toPlannerActivityDateTime
+import com.rocketseat.planner.domain.utils.toPlannerActivityTime
 import com.rocketseat.planner.presentation.ui.component.PlannerActivityAdapter
 import com.rocketseat.planner.presentation.ui.component.PlannerActivityDatePickerDialogFragment
 import com.rocketseat.planner.presentation.ui.component.PlannerActivityTimePickerDialogFragment
+import com.rocketseat.planner.presentation.ui.extension.hideKeyboard
 import com.rocketseat.planner.presentation.ui.viewmodel.PlannerActivityViewModel
+import com.rocketseat.planner.presentation.ui.viewmodel.SetDate
+import com.rocketseat.planner.presentation.ui.viewmodel.SetTime
 import com.rocketseat.planner.presentation.ui.viewmodel.UserRegistrationViewModel
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
@@ -44,10 +52,38 @@ class HomeFragment : Fragment() {
         with(binding) {
             plannerActivityViewModel.fetchActivities()
 
+            clHomeContainer.setOnClickListener {
+                tietPlannerActivityName.clearFocus()
+                requireContext().hideKeyboard(fromView = tietPlannerActivityName)
+            }
+
+            tietPlannerActivityName.doOnTextChanged{ text, _, _, _ ->
+                if(text.toString().isEmpty()) {
+                    tietPlannerActivityName.clearFocus()
+                    requireContext().hideKeyboard(fromView = tietPlannerActivityName)
+                }
+                plannerActivityViewModel.updateNewActivity(
+                    name = text.toString()
+                )
+            }
+
             tietNewPlannerActivityDate.setOnClickListener {
                 PlannerActivityDatePickerDialogFragment(
-                    onConfirm = { year, month, day ->
-                        Toast.makeText(requireContext(), "$year, $month, $day", Toast.LENGTH_SHORT).show()
+                    onConfirm = { year, month, dayOfMonth ->
+                        val filledCalendar = Calendar.getInstance().apply {
+                            set(Calendar.YEAR, year)
+                            set(Calendar.MONTH, month)
+                            set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                        }
+
+                        tietNewPlannerActivityDate.setText(filledCalendar.toPlannerActivityDate())
+                        plannerActivityViewModel.updateNewActivity(
+                            date = SetDate(
+                                year = year,
+                                month = month,
+                                dayOfMonth = dayOfMonth
+                            )
+                        )
                     },
                     onCancel = {}
                 ).show(
@@ -59,7 +95,18 @@ class HomeFragment : Fragment() {
             tietNewPlannerActivityTime.setOnClickListener {
                 PlannerActivityTimePickerDialogFragment(
                     onConfirm = { hourOfDay, minute ->
-                        Toast.makeText(requireContext(), "$hourOfDay $minute", Toast.LENGTH_SHORT).show()
+                        val filledCalendar = Calendar.getInstance().apply {
+                            set(Calendar.HOUR_OF_DAY, hourOfDay)
+                            set(Calendar.MINUTE, minute)
+                        }
+
+                        tietNewPlannerActivityTime.setText(filledCalendar.toPlannerActivityTime())
+                        plannerActivityViewModel.updateNewActivity(
+                            time = SetTime(
+                                hourOfDay = hourOfDay,
+                                minute = minute
+                            )
+                        )
                     },
                     onCancel = {}
                 ).show(
@@ -69,12 +116,27 @@ class HomeFragment : Fragment() {
             }
 
             btnSaveNewPlannerActivity.setOnClickListener {
-                UpdatePlannerActivityDialogFragment().show(
-                    childFragmentManager,
-                    UpdatePlannerActivityDialogFragment.TAG
+                plannerActivityViewModel.saveNewActivity(
+                    onSuccess = {
+                        clearNewPlannerActivityFields()
+                    },
+                    onError = {
+                        Toast.makeText(
+                            requireContext(),
+                            getString(R.string.oops_houve_uma_falha_ao_salvar_a_atividade),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 )
             }
         }
+    }
+
+    private fun FragmentHomeBinding.clearNewPlannerActivityFields() {
+        tietPlannerActivityName.text = null
+        tietNewPlannerActivityDate.text = null
+        tietNewPlannerActivityTime.text = null
+        requireContext().hideKeyboard(fromView = tietPlannerActivityName)
     }
 
     private fun setupObservers() {
---
+++ app/src/main/java/com/rocketseat/planner/presentation/ui/extension/ContextExtension.kt
@@ -0,0 +1,11 @@
+package com.rocketseat.planner.presentation.ui.extension
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Context.hideKeyboard(fromView: View) {
+    val inputMethodManager =
+        this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(fromView.windowToken, 0)
+}

ac90460d2a713246bccadb85c4fb56ecb58e9689
Egito Filho <egitofilho4@gmail.com>
2025-02-08 11:59:34 -0300

    05_07_CONHECENDO_OS_PRINCIPIOS_SOLID_PARTE_02

--- app/src/main/java/com/rocketseat/planner/data/UserManager.kt
+++ app/src/main/java/com/rocketseat/planner/data/UserManager.kt
@@ -14,16 +14,47 @@ class UserManager {
 }
 
 // OK
-class UserService {
 
-    fun registerUser(name: String, email: String) {
+interface Service {
+    val baseUrl: String
+    fun run()
+
+    // X - segregação de interface
+//    fun registerUser(name: String, email: String)
+//    fun sendEmail(email: String, message: String)
+}
+
+// OK - segregação de interface
+
+interface UserServiceAction {
+    fun registerUser(name: String, email: String)
+
+    // ..
+}
+
+interface EmailServiceAction {
+    fun sendEmail(email: String, message: String)
+
+    // ..
+}
+
+class UserService(override val baseUrl: String) : Service, UserServiceAction {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+
+    override fun registerUser(name: String, email: String) {
         // ...
     }
 }
 
-class EmailService {
+class EmailService(override val baseUrl: String) : Service, EmailServiceAction {
 
-    fun sendEmail(email: String, message: String) {
+    override fun sendEmail(email: String, message: String) {
         // ...
+    }
+
+    override fun run() {
+        TODO("Not yet implemented")
     }
 }
